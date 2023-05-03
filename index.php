e68994bde6a414edbff8bc7b66db5d718f640895
Atikah <atikahsyifaa@gmail.com>
2023-05-03 23:59:28 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>My Profile</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+</head>
+
+<body style="font-family: 'Poppins', sans-serif;">
+    <div class="text-bg-light p-3 text-center h3">My Profile</div>
+    <div class="text-bg-secondary p-3 text-center">
+        <img class="object-fit-none rounded-circle py-3" style="width: 15%;" src="https://storage.googleapis.com/profile-app/pic.jpg">
+        <h4 class="text-center">Atikah Syifa Warastri
+        </h4>
+        <p class="text-center">Cloud Computing Student</p>
+    </div>
+    <div class="row mw-100 p-5">
+        <div class="col-8 px-5">
+            <div class="mx-auto px-5">
+                <h5 class="border-bottom border-danger border-3">Hi!</h5>
+                <p class="pt-4">I am a Sriwijaya University third year student majoring in Informatics Engineering.
+                    Currently a
+                    Bangkit 2023 Cloud Computing student. I am interested in Web Development, especially Frontend
+                    Development.</p>
+                <p>Being a Bangkit Student makes me understand and apply Time Management into daily basis. Fortunately,
+                    Bangkit provides soft skill learning which can be found every 2 weeks on ILT Session.</p>
+            </div>
+        </div>
+        <div class="col px-5">
+            <p class="pb-5 fw-semibold">You can reach me out here</p>
+            <div class="row border-bottom border-secondary border-2">
+                <p class="col-3">email</p>
+                <a class="col" href="mailto:atikahsyifaa@gmail.com">atikahsyifaa@gmail.com</a>
+            </div>
+            <div class="row border-bottom border-secondary border-2 pt-3">
+                <p class="col-3">linkedin</p>
+                <a class="col" href="https://www.linkedin.com/in/atikah-syifa-warastri/">atikahsyf</a>
+            </div>
+            <div class="row border-bottom border-secondary border-2 pt-3">
+                <p class="col-3">github</p>
+                <a class="col" href="https://github.com/atikahsyf">atikahsyf</a>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
